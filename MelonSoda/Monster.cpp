f5ae00c72762feed800ac89784f076028eae5070
seoyanalt4 <seoyanalt4@naver.com>
2025-05-06 22:57:23 +0900

    Update Monster.cpp_0506_2257

--- MelonSoda/Monster.cpp
+++ MelonSoda/Monster.cpp
@@ -14,3 +14,8 @@ void Monster::showMStat() {
 	std::cout << "Name : " << name << std::endl;
 	std::cout << "HP : " << HP << std::endl;
 }
+
+//몬스터 정의 보스전 직전 몬스터한테만 마지막에 매개변수 추가하기 ex) const Montser preBoss("보스 직전 몬스터", 50, 4, 5, 1, 3, 5);
+const Monster slime("슬라임", 24, 0, 5, 3);
+const Monster king_slime("킹슬라임", 37, 0, 8, 7);
+const Monster chandelier("샹들리에", 61, 6, 13, 10);
